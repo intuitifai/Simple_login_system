5f8d14ec35477243d4e96f28a5f0e6f61dfe5d67
Rahul Kumar <rahulkr9622@gmail.com>
2021-01-02 19:56:40 +0530

    Updated mysql file
    Updated the indentation

--- mysql_script.sql
+++ mysql_script.sql
@@ -2,7 +2,7 @@ create database IF NOT EXISTS mydb;
 use mydb;
 DROP TABLE IF EXISTS login_system;
 create table login_system (
-	id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
     email VARCHAR(100),
     password NVARCHAR(100),
